3f5c9e0fe0a01388f5eea0c45c51664f06070234
feng.dev <feng@vitruvio.cl>
2025-06-24 18:39:52 -0400

    Update main.go

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -2,9 +2,76 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/fengdotdev/golibs-remotefn/sandbox/draf2/remote"
 )
+
+func op(key string, data []byte) []byte {
+
+	errmap := remote.ResultAndErr(1, errors.New("some error"))
+	m, err := remote.DataToMapOnJson(data)
+	if err != nil {
+		outdata, err := remote.MapToDataOnJson(errmap)
+		if err != nil {
+			panic(fmt.Sprintf("Error converting errlr to map: %v", err))
+		}
+		return outdata
+	}
+
+	result, err := Foohandler(context.Background(), m)
+	if err != nil {
+		outdata, err := remote.MapToDataOnJson(remote.ResultAndErr(0, err))
+		if err != nil {
+			panic(fmt.Sprintf("Error converting error to data: %v", err))
+		}
+		return outdata
+	}
+	outdata, err := remote.MapToDataOnJson(result)
+	if err != nil {
+		panic(fmt.Sprintf("Error converting result to data: %v", err))
+	}
+	return outdata
+}
+
+func FooRemote(a, b int) (int, error) {
+	ctx := context.Background()
+	m := map[string]interface{}{
+		"a": a,
+		"b": b,
+	}
+	data, err := remote.MapToDataOnJson(m)
+	if err != nil {
+		return 1, err
+	}
+	dataChan := make(chan []byte)
+
+	returndata := remote.MockWire(ctx, dataChan, "Foo", op)
+
+	dataChan <- data
+	select {
+	case result := <-returndata:
+		resultMap, err := remote.DataToMapOnJson(result)
+		if err != nil {
+			return 0, fmt.Errorf("error converting data to map: %v", err)
+		}
+		if res, ok := resultMap["result"]; ok {
+
+			// Check if the result is of type int
+			if res == nil {
+				return 0, fmt.Errorf("result is nil")
+			}
+			if _, ok := res.(int); !ok {
+				return 0, fmt.Errorf("result is not of type int, got %T", res)
+			}
+
+			return res.(int), nil
+		}
+
+		return 1, nil
+	}
+}
 
 func Foo(a, b int) int {
 	return a + b
@@ -12,8 +79,15 @@ func Foo(a, b int) int {
 
 func Foohandler(ctx context.Context, m map[string]interface{}) (map[string]interface{}, error) {
 
-	a := remote.Or(m, "a", 0)
-	b := remote.Or(m, "b", 0)
+	a, err := remote.OrErr[int](m, "a")
+	if err != nil {
+		return remote.ResultAndErr(0, fmt.Errorf("error getting 'a': %w", err)), nil
+	}
+
+	b, err := remote.OrErr[int](m, "b")
+	if err != nil {
+		return remote.ResultAndErr(0, fmt.Errorf("error getting 'b': %w", err)), nil
+	}
 
 	result := Foo(a, b)
 
@@ -24,35 +98,10 @@ func Foohandler(ctx context.Context, m map[string]interface{}) (map[string]inter
 
 func main() {
 
-	reg := remote.NewGoRegister()
-
-	err := reg.RegisterRemoteFn("Foo", Foohandler)
+	result, err := FooRemote(1, 2)
 	if err != nil {
-		panic(err)
+		panic(fmt.Sprintf("Error in FooRemote: %v", err))
 	}
-
-
-
-	
-
-
-	fn, err := reg.GetRemoteFn("Foo")
-	if err != nil {
-		panic(err)
-	}
-
-	ctx := context.Background()
-	m := map[string]interface{}{
-		"a": 5,
-		"b": 10,
-	}
-
-	result, err := fn(ctx, m)
-	if err != nil {
-		panic(err)
-	}
-	if res, ok := result["result"]; ok {
-		println("Result:", res.(int))
-	}
+	println("Result:", result)
 
 }
